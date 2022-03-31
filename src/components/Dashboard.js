352d331cb6b32866470b8b685744c685cf2c3479
Pablo Tack <tackpablo@gmail.com>
2022-03-30 19:11:15 -0700

    add state for rendering 2 views

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,20 +27,36 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false };
+  state = { loading: false, focused: null };
+
+  selectPanel = (id) => {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  };
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map((panel) => {
-      const { id, label, value } = panel;
-      return <Panel key={id} id={id} label={label} value={value} />;
-    });
-
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={(event) => this.selectPanel(panel.id)}
+        />
+      ));
     return <main className={dashboardClasses}>{panels}</main>;
   }
 }
--- src/helpers/reducers.js
+++ src/helpers/reducers.js
@@ -1,11 +1,11 @@
 import { filter } from "lodash/fp";
 
 function getSpotsForDay(state, day) {
-  const foundDay = state.days.find(d => d.name === day);
+  const foundDay = state.days.find((d) => d.name === day);
 
   if (!foundDay) throw new Error(`No Day Found: ${day}`);
 
-  return filter(id => state.appointments[id].interview === null)(
+  return filter((id) => state.appointments[id].interview === null)(
     foundDay.appointments
   ).length;
 }
@@ -15,16 +15,16 @@ export function setInterview(state, id, interview) {
     ...state.appointments,
     [id]: {
       ...state.appointments[id],
-      interview
-    }
+      interview,
+    },
   };
 
   return {
     ...state,
-    days: state.days.map(day => ({
+    days: state.days.map((day) => ({
       ...day,
-      spots: getSpotsForDay({ ...state, appointments }, day.name)
+      spots: getSpotsForDay({ ...state, appointments }, day.name),
     })),
-    appointments
+    appointments,
   };
 }
